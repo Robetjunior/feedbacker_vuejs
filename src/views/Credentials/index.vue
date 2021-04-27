90c4e5a1aef9fbb7bc9167a86e30c1d5ab51b618
Jose Roberto <juniorferreirarob@gmail.com>
2021-04-27 15:44:46 -0300

    update Credentials/index.vue

--- src/views/Credentials/index.vue
+++ src/views/Credentials/index.vue
@@ -14,18 +14,56 @@
     </div>
 
      <div class="flex justify-center w-full h-full">
-      <div class="flex flex-col items-center w-4/5 max-w-6xl py-10">
+      <div class="flex flex-col w-4/5 max-w-6xl py-10">
         <h1 class="text-3xl font-black text-brand-darkgray">
           Instalação e configuração
         </h1>
-        <p class="mt-10 text-lg text-center text-gray-800 font-regular">
+        <p class="mt-10 text-lg text-gray-800 font-regular">
           Este aqui é a sua chave de api
         </p>
 
+        <content-loader
+          v-if="store.Global.isLoading || state.isLoading"
+          class="rounded"
+          width="600px"
+          height="50px"
+        />
         <div
-          class="flex py-3 pl-5 pr-20 mt-2 roudend items-center bg-brand-gray"
+          v-else
+          class="flex py-3 pl-5 pr-5 mt-2 roudend justify-between items-center bg-brand-gray w-full lg:w-1/2"
         >
-          <!-- <span>{{ store.User.currentUser.apiKey }}</span> -->
+          <span>{{ store.User.currentUser.apiKey }}</span>
+          <div class="flex ml-20 mr-5">
+            <icon
+              name="copy"
+              :color="brandColors.graydark"
+              size="24"
+              class="cursor-pointer"
+            />
+            <icon
+              name="loading"
+              :color="brandColors.graydark"
+              size="24"
+              class="cursor-pointer ml-3"
+            />
+          </div>
+        </div>
+
+        <p class="mt-5 text-lg text-gray-800 font-regular">
+          Coloque o script abaixo no seu site para começar a receber feedbacks
+        </p>
+
+        <content-loader
+          v-if="store.Global.isLoading || state.isLoading"
+          class="rounded"
+          width="600px"
+          height="50px"
+        />
+        <div
+          v-else
+          class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
+        >
+          <pre>&lt;script src="https://Robetjunior-feedbacker-widget.netlify.app?apiKey={{store.User.currentUser.apiKey}}"&gt;&lt;/script&gt;</pre>
         </div>
       </div>
     </div>
@@ -34,9 +72,20 @@
 
 <script>
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
+import Icon from '../../components/Icon'
+import useStore from '../../hooks/useStore'
+import pallete from '../../../palette'
 
 export default {
-  components: { HeaderLogged }
+  components: { ContentLoader, HeaderLogged, Icon },
+  setup () {
+    const store = useStore()
 
+    return {
+      store,
+      brandColors: pallete.brand
+    }
+  }
 }
 </script>

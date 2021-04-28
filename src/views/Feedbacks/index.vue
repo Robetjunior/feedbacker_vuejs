be034910302c2d1f9968f1f87e978ed04587331c
Jose Roberto <juniorferreirarob@gmail.com>
2021-04-28 12:18:29 -0300

    create view/Filters

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -12,6 +12,19 @@
         Detalhes de todos os feedbacks recebidos.
       </p>
     </div>
+
+    <div class="flex justify-center w-full pb-20">
+      <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+        <div>
+          <h1 class="text-3xl font-black text-brand-darkgray">
+            Listagem
+          </h1>
+        </div>
+        <div class="px-10 pt-20 col-span-3">
+
+        </div>
+      </div>
+    </div>
 </div>
 </template>
 

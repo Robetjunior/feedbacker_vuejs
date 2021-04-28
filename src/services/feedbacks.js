220b1c62ddb4971eb515914b030b93caa381fb94
Jose Roberto <juniorferreirarob@gmail.com>
2021-04-28 11:48:52 -0300

    create services/feedbacks

---
+++ src/services/feedbacks.js
@@ -0,0 +1,20 @@
+const defaultPagination = {
+  limit: 5,
+  offset: 0
+};
+
+export default httpClient => ({
+  getAll: async ({ type, limit, offset } = defaultPagination) => {
+    const query = { limit, offset };
+    if (type) {
+      query.type = type;
+    }
+
+    const respose = await httpClient.get("/feedbacks", { params: query });
+    return { data: respose.data };
+  },
+  getSummary: async () => {
+    const response = await httpClient.get("/feedbacks/summary");
+    return { data: response.data };
+  }
+});
